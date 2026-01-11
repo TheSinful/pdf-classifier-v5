b3d3d5e0eb685ece99087b33eeaf2de2445376a9
WhiteKSI <email@gmail.com>
2026-01-11 11:56:06 -0600

    old placeholder files

--- data/test.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <any>
-#include <mupdf/fitz.h>
-#include <shared/result.h>
-
-
-Result* classify(fz_context* ctx, fz_document* doc);
-Result* extract(fz_context* ctx, fz_document* doc, void* shared);
