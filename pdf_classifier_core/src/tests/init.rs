e3d42a862db23afe7af5b60bff97f27a47b616d8
WhiteKSI <email@gmail.com>
2026-03-22 15:42:44 -0500

    Define TEST_OUTPUT_DIR test path, and update macro to include support for directories.

--- pdf_classifier_core/src/tests/init.rs
+++ pdf_classifier_core/src/tests/init.rs
@@ -6,6 +6,7 @@ fn init_tests() {
     init_logger();
     set_SMALL_TEST_DOC_test_path();
     set_LARGE_TEST_DOC_test_path();
+    set_TEST_OUTPUT_DIR_test_path();
 }
 
 macro_rules! get_root_path {
@@ -31,14 +32,42 @@ macro_rules! def_test_path {
 
             #[allow(non_snake_case)]
             pub fn [<get_ $name _test_path>]() -> PathBuf {
-                get_root_path!().join(stringify!($path))
+                get_root_path!().join($path)
+            }
+        }
+    };
+
+    ($name: ident, $path: expr, $dir: expr) => {
+        paste! {
+            const $name: &'static str = $path;
+
+            #[allow(non_snake_case)]
+            fn [<set_ $name _test_path>]() -> () {
+                let path = get_root_path!().join($path);
+
+                if !path.exists() {
+                    std::fs::create_dir(path.clone()).expect(&format!("failed to create test directory {} at path {}", stringify!($name), path.clone().to_string_lossy().to_string()));
+                }
+
+                unsafe {
+                    std::env::set_var(
+                        stringify!($name),
+                        path
+                    )
+                }
+            }
+
+            #[allow(non_snake_case)]
+            pub fn [<get_ $name _test_path>]() -> PathBuf {
+                get_root_path!().join($path)
             }
         }
     };
 }
 
-def_test_path!(SMALL_TEST_DOC, "data/small_test_doc.pdf");
-def_test_path!(LARGE_TEST_DOC, "data/large_test_doc.pdf");
+def_test_path!(SMALL_TEST_DOC, "../data/small_test_doc.pdf");
+def_test_path!(LARGE_TEST_DOC, "../data/large_test_doc.pdf");
+def_test_path!(TEST_OUTPUT_DIR, "target/classifier_tests", true);
 
 fn init_logger() {
     env_logger::builder()
