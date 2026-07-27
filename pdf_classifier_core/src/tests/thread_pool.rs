aec5fb60a558dca715f644c2c58d1f55d3da8c35
WhiteKSI <the3sinful@gmail.com>
2026-07-27 06:13:24 -0500

    Create more specific threadpool tests for each type of poll.

--- pdf_classifier_core/src/tests/thread_pool.rs
+++ pdf_classifier_core/src/tests/thread_pool.rs
@@ -3,66 +3,63 @@ use crate::generated::generated_object_types::KnownObject;
 use crate::page::Page;
 use crate::tests::init::get_LARGE_TEST_DOC_test_path;
 use crate::threading::pool::*;
+use std::unreachable;
+
+fn make_pool() -> ThreadPool {
+    ThreadPool::new(4, get_LARGE_TEST_DOC_test_path())
+}
+
+fn expect_result(of_class: KnownObject, result: Option<JobResult>) {
+    assert!(result.is_some());
+
+    match result.unwrap() {
+        JobResult::Classification {
+            page: _,
+            res: _,
+            as_class,
+        } => assert_eq!(as_class, of_class),
+        JobResult::Extraction {
+            page: _,
+            res: _,
+            as_class: _,
+        } => unreachable!("shouldn't get an extraction case when only scheduling classifications."),
+    }
+}
+
+const FIRST_PAGE: Page = Page(FIRST_CHAPTER_PAGE);
 
 #[test]
-fn test_poll() {
-    let mut pool = ThreadPool::new(4, get_LARGE_TEST_DOC_test_path());
-    pool.classify(KnownObject::CHAPTER, Page(FIRST_CHAPTER_PAGE));
+fn test_poll_blocking() {
+    let mut pool = make_pool();
 
-    let mut _prev = 0;
-    let mut _classified = false;
-    loop {
-        _prev += 3;
+    pool.classify(KnownObject::CHAPTER, FIRST_PAGE);
+    pool.classify(KnownObject::SUBCHAPTER, FIRST_PAGE);
 
-        let mut results = pool.poll().expect("thread pool shouldn't be exhausted");
+    expect_result(KnownObject::CHAPTER, pool.poll_blocking()); // should be the first available
+    expect_result(KnownObject::SUBCHAPTER, pool.poll_blocking()); // should be available after blocking again
+    assert!(pool.poll_blocking().is_none()); // check for exhaustion
+}
 
-        if results.is_empty() {
-            continue; // awaiting  
-        }
+#[test]
+fn test_poll_draining() {
+    let mut pool = make_pool();
 
-        let res = results.pop().unwrap();
-
-        match res {
-            JobResult::Classification {
-                page,
-                res,
-                as_class,
-            } => {
-                assert!(
-                    res.is_ok(),
-                    "Classification failed {}",
-                    res.as_ref().err().unwrap()
-                );
-
-                dbg!(
-                    "Classified page {} as class {}",
-                    page.0,
-                    as_class.to_string()
-                );
-
-                _classified = true;
-            }
-            JobResult::Extraction {
-                page,
-                res,
-                as_class,
-            } => {
-                assert!(
-                    res.is_ok(),
-                    "Extraction failed {}",
-                    res.err().unwrap().extract_fail_rsn()
-                );
-
-                dbg!(
-                    "Extracted page {} as class {}",
-                    page.0,
-                    as_class.to_string()
-                );
-
-                break;
-            }
-        }
-
-        assert!(_classified)
+    for i in 0..100 {
+        pool.classify(KnownObject::CHAPTER, Page(i));
     }
+
+    let poll = pool.poll_draining();
+
+    let mut total_classify_jobs_polled = 0;
+    for res in poll {
+        if let JobResult::Classification {
+            page: _,
+            res: _,
+            as_class: _,
+        } = res
+        {
+            total_classify_jobs_polled += 1;
+        }
+    }
+    assert_eq!(total_classify_jobs_polled, 100);
 }
