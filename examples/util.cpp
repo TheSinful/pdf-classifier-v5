21fdf2d0481cd861e0cf9e40e00dbd73067f90ec
WhiteKSI <the3sinful@gmail.com>
2026-08-04 11:39:07 -0500

    Begins integration with pdf_classifer_lib within examples.

--- examples/util.cpp
+++
@@ -1,7 +0,0 @@
-#include <util.hpp>
-
-void deleter_StdString(void* ptr) { delete static_cast<std::string*>(ptr); }
-Result* json_to_payload(nlohmann::json json_obj) {
-  std::string* payload = new std::string(json_obj.dump());
-  return Result::ok(static_cast<void*>(payload), deleter_StdString);
-}
