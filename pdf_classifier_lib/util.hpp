a33290c1e0ada720a47414122747b6df398fd3f4
WhiteKSI <the3sinful@gmail.com>
2026-08-07 21:12:46 -0500

    Add legacy `UNWRAP_RESULT` and `PDF_ASSERT` macros within pdf_classifier_lib. Rename both to reflect them as legacy macros.

--- pdf_classifier_lib/util.hpp
+++ pdf_classifier_lib/util.hpp
@@ -7,3 +7,26 @@
         if (!_unwrap_res.is_ok())  \
             return _unwrap_res;    \
     } while (0)
+
+#define UNWRAP_RAW_RESULT(res)                 \
+    do                                         \
+    {                                          \
+        Result *__res = res;                   \
+        if (__res->type == Result::Type::FAIL) \
+        {                                      \
+            return __res;                      \
+        }                                      \
+        else                                   \
+        {                                      \
+            delete __res;                      \
+        }                                      \
+    } while (0)
+
+#define ASSERT_RAW_RESULT(condition, fmt, ...)                  \
+    do                                                          \
+    {                                                           \
+        if (condition)                                          \
+        {                                                       \
+            return Result::fail(std::format(fmt, __VA_ARGS__)); \
+        }                                                       \
+    } while (0)
